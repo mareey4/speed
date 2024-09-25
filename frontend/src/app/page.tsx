c11d5a4555e960549b595d67679aa715aab703de
Maria Paule <m4riapaul3@gmail.com>
2024-09-25 23:15:53 +1200

    feat: added the main page title and paragraphs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,16 @@
-'use client'
-
-import ShowBookList from "@/components/ShowBookList";
 
 export default function Home() {
+
   return (
-    <main>
-      <ShowBookList />
-    </main>
-  )
+    <div className="min-h-screen">
+      <main className="container mx-auto p-8">
+        <h1 className="text-4xl font-bold text-center">Welcome to SPEED!</h1>
+        <p className="mt-4 text-center">
+          Your go-to searchable database for empirical software engineering evidence.
+          <br />
+          The Software Empirical Evidence Database is the best ever!
+        </p>
+      </main>
+    </div>
+  );
 }
