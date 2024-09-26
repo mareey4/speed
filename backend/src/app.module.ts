458ac006c5d3efa5e8a6084bcca1e68e258d64f9
Maria Paule <m4riapaul3@gmail.com>
2024-09-26 21:52:05 +1200

    fix: uncommented out imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-// import { BookModule } from './api/books/book.module';
+import { BookModule } from './api/books/book.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
-    // BookModule,
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
