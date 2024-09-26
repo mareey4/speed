b2cfa99d2722a13405d08ad906912d80562395dd
Maria Paule <m4riapaul3@gmail.com>
2024-09-26 21:51:40 +1200

    fix: moved CreateBookComponents to user folder

---
+++ frontend/src/app/user/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import CreateBookComponent from "@/components/CreateBookComponent";
+
+export default function CreateBook() {
+  return (
+    <main>
+      <CreateBookComponent />
+    </main>
+  );
+}
---
+++ frontend/src/components/CreateBookComponent.tsx
@@ -0,0 +1,130 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { Book, DefaultEmptyBook } from "./Book";
+
+const CreateBookComponent = () => {
+  const navigate = useRouter();
+  const [book, setBook] = useState<Book>(DefaultEmptyBook);
+
+ const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setBook({ ...book, [event.target.name]: event.target.value });
+  };
+
+   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(book);
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books`, {method: 'POST', headers: {"Content-Type": "application/json"}, body: JSON.stringify(book)})
+    .then((res) => {
+        console.log(res);
+        setBook(DefaultEmptyBook);
+        // Push to /
+        navigate.push("/");
+      })
+      .catch((err) => {
+        console.log('Error from CreateBook: ' + err);
+      });
+  };
+
+  const buttonClass = "bg-blue-600 text-white p-2 w-24 h-15 flex items-center justify-center rounded-lg hover:bg-blue-700 transition";
+
+  return (
+    <div className="CreateBook" style={{ backgroundColor: 'black', minHeight: '100vh', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <br/>
+            <Link href="/" className={buttonClass}>
+                  Show Book List
+            </Link>
+            </div>
+            <div className="col-md-10 m-auto">
+              <h1 className="text-6xl font-bold">Add Book</h1>
+              <p className="text-3xl font-semibold mt-2">Create new book</p>
+              <form noValidate onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Title of the Book"
+                  name="title"
+                  className="form-control"
+                  value={book.title}
+                  onChange={onChange}
+                  style={{ color: 'black', backgroundColor: 'white', borderColor: 'white' }}
+                />
+              </div>
+              <br />
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="ISBN"
+                  name="isbn"
+                  className="form-control"
+                  value={book.isbn}
+                  onChange={onChange}
+                  style={{ color: 'black', backgroundColor: 'white', borderColor: 'white' }}
+                />
+              </div>
+              <br />
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Author"
+                  name="author"
+                  className="form-control"
+                  value={book.author}
+                  onChange={onChange}
+                  style={{ color: 'black', backgroundColor: 'white', borderColor: 'white' }}
+                />
+              </div>
+              <br />
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Describe this book"
+                  name="description"
+                  className="form-control"
+                  value={book.description}
+                  onChange={onChange}
+                  style={{ color: 'black', backgroundColor: 'white', borderColor: 'white' }}
+                />
+              </div>
+              <br />
+              <div className="form-group">
+                <input
+                  type="date"
+                  name="published_date"
+                  className="form-control"
+                  value={book.published_date?.toString()}
+                  onChange={onChange}
+                  style={{ color: 'black', backgroundColor: 'white', borderColor: 'white' }}
+                />
+              </div>
+              <br />
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Publisher of this Book"
+                  name="publisher"
+                  className="form-control"
+                  value={book.publisher}
+                  onChange={onChange}
+                  style={{ color: 'black', backgroundColor: 'white', borderColor: 'white' }}
+                />
+              </div>
+              <br />
+              <button
+                type="submit"
+                className={buttonClass + " w-full"}
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBookComponent;
