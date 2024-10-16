a2db8f56da36f36db79cbbbddd768b4a6222572c
Maria Paule <m4riapaul3@gmail.com>
2024-10-16 23:52:34 +1300

    fix: added titles

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -4,8 +4,14 @@ import AdminView from "@/components/admin/AdminView";
 
 export default function CreateBook() {
   return (
-    <main>
-      <AdminView />
-    </main>
+    <div className="min-h-screen">
+      <main className="container mx-auto p-8">
+        <h1 className="text-4xl font-bold text-center">Admin</h1>
+        <p className="mt-4 text-center">
+          Edit however you like! <br />
+        </p>
+        <AdminView />
+      </main>
+    </div>
   );
 }
