c28c6c74d84bd133067875115bc58c513f70ecdf
Maria Paule <m4riapaul3@gmail.com>
2024-10-20 00:21:21 +1300

    fix: removed error about aptos

--- frontend/src/app/moderator/page.tsx
+++ frontend/src/app/moderator/page.tsx
@@ -8,7 +8,7 @@ export default function ModeratorPage() {
       <main className="container mx-auto p-8">
         <h1 className="text-4xl font-bold text-center">Moderate an Article</h1>
         <p className="mt-4 text-center">
-          Once a user has submitted an article, please choose if it's approved or rejected!
+          Once a user has submitted an article, please choose if it can be approved or rejected!
         </p>
         <ShowToAnalyse isModerator={true} />
       </main>
